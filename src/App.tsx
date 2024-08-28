2751659ecf5339e499a43ac6c340d923efbf291f
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2024-08-28 23:54:02 +0300

    Add light theme to the organisation page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Organisation from './pages/organisation/Organisation';
 function App() {
   return (
     <ThemeProvider>
-      <main className="w-screen text-black dark:text-dark-300 flex justify-center align-middle">
+      <main className="w-screen text-black dark:text-dark-300 flex justify-center align-middle bg-dark-100 dark:bg-black">
         <Sidebar />
         <div className="flex-1 p-10">
           <Organisation />
--- src/pages/organisation/Organisation.tsx
+++ src/pages/organisation/Organisation.tsx
@@ -3,29 +3,35 @@ import image from './img/download.png';
 
 function Organisation() {
   return (
-    <>
+    <section className="text-gray-600 dark:text-white">
       {/* Header Section */}
       <div className="relative p-4 bg-white dark:bg-dark-950 rounded-2xl">
         <div className="bottom-4 left-4">
           <div className="flex flex-row justify-between">
             <div className="ml-4">
-              <h1 className="text-2xl font-bold text-white">
-                Test organisation
-              </h1>
-              <p className="text-gray-300">ALL MEMBERS WELCOME!</p>
+              <h1 className="text-2xl font-bold">Test organisation</h1>
+              <p className="text-indigo-400 dark:text-dark-300">
+                ALL MEMBERS WELCOME!
+              </p>
             </div>
             <div className="flex space-x-8">
               <div className="flex flex-col justify-center items-center gap-2">
-                <p className="font-bold text-xl text-white">139</p>
-                <p className="font-bold text-xs">MEMBERS</p>
+                <p className="font-bold text-xl text-indigo-800 dark:text-white">
+                  139
+                </p>
+                <p className="font-bold text-xs dark:text-dark-200">MEMBERS</p>
               </div>
               <div className="flex flex-col justify-center items-center gap-2">
-                <p className="font-bold text-xl text-white">105</p>
-                <p className="font-bold text-xs">POSTS</p>
+                <p className="font-bold text-xl text-indigo-800 dark:text-white">
+                  105
+                </p>
+                <p className="font-bold text-xs dark:text-dark-200">POSTS</p>
               </div>
               <div className="flex flex-col justify-center items-center gap-2">
-                <p className="font-bold text-xl text-white">7.3k</p>
-                <p className="font-bold text-xs">VISITS</p>
+                <p className="font-bold text-xl text-indigo-800 dark:text-white">
+                  7.3k
+                </p>
+                <p className="font-bold text-xs dark:text-dark-200">VISITS</p>
               </div>
               <div className="flex gap-3 items-center ">
                 <button className="h-full px-4 rounded-lg bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800 dark:from-dark-700 dark:to-dark-600 dark:text-dark-200">
@@ -42,11 +48,11 @@ function Organisation() {
 
       {/* Main Content Section */}
       <div className="pt-4">
-        <div className="flex justify-center space-x-4 text-black dark:text-white">
+        <div className="flex justify-center space-x-4">
           {/* Sidebar */}
           <div className="w-1/4">
             {/* Social Networks */}
-            <div className="mb-4 flex flex-col space-y-7 text-white bg-white dark:bg-dark-950 p-4 rounded-lg">
+            <div className="mb-4 flex flex-col space-y-7 bg-white dark:bg-dark-950 p-4 rounded-lg">
               <div className="flex justify-between items-center mb-2">
                 <h2 className="text-lg font-bold">Group Info</h2>
                 <button className="h-full p-4 rounded-lg bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800 dark:from-dark-700 dark:to-dark-600 dark:text-dark-200">
@@ -62,20 +68,26 @@ function Organisation() {
               <div className="flex flex-col space-y-3">
                 <div className="w-full flex justify-between items-center">
                   <span className="dark:text-dark-300">Email</span>
-                  <span>stefanptrg@gmail.com</span>
+                  <span className="text-indigo-800 dark:text-white">
+                    stefanptrg@gmail.com
+                  </span>
                 </div>
                 <div className="w-full flex justify-between items-center">
                   <span className="dark:text-dark-300">Location</span>
-                  <span>Targovishte, Bulgaria</span>
+                  <span className="text-indigo-800 dark:text-white">
+                    Targovishte, Bulgaria
+                  </span>
                 </div>
                 <div className="w-full flex justify-between items-center">
                   <span className="dark:text-dark-300">Created</span>
-                  <span>April 9th, 2018</span>
+                  <span className="text-indigo-800 dark:text-white">
+                    April 9th, 2018
+                  </span>
                 </div>
               </div>
             </div>
-            <div className="mb-4 flex flex-col space-y-7 text-white bg-white dark:bg-dark-950 p-4 rounded-lg">
-              <h2 className="text-lg font-bold mb-2">Group Organizers</h2>
+            <div className="mb-4 flex flex-col space-y-7 bg-white dark:bg-dark-950 p-4 rounded-lg">
+              <h2 className="text-lg font-bold">Group Organizers</h2>
               <div className="flex items-center mb-4">
                 <img
                   src={image}
@@ -84,7 +96,9 @@ function Organisation() {
                 />
                 <div className="ml-4">
                   <p className="font-bold">Marina Valentine</p>
-                  <p className="text-gray-400">2 friends in common</p>
+                  <p className="text-indigo-800 dark:text-dark-300">
+                    2 friends in common
+                  </p>
                 </div>
               </div>
               <div className="flex items-center">
@@ -95,7 +109,9 @@ function Organisation() {
                 />
                 <div className="ml-4">
                   <p className="font-bold">Neko Bebop</p>
-                  <p className="text-gray-400">1 friend in common</p>
+                  <p className="text-indigo-800 dark:text-dark-300">
+                    1 friends in common
+                  </p>
                 </div>
               </div>
             </div>
@@ -103,7 +119,7 @@ function Organisation() {
           {/* Main Content */}
           <div className="w-3/4">
             {/* Post */}
-            <div className="mb-4 flex flex-col space-y-7 text-white bg-white dark:bg-dark-950 p-4 rounded-lg">
+            <div className="mb-4 flex flex-col space-y-7 bg-white dark:bg-dark-950 p-4 rounded-lg">
               <div className="flex items-center mb-2">
                 <img
                   src={image}
@@ -112,7 +128,9 @@ function Organisation() {
                 />
                 <div className="ml-4">
                   <p className="font-bold">Neko Bebop</p>
-                  <p className="text-gray-400">2 weeks ago</p>
+                  <p className="text-indigo-800 dark:text-dark-300">
+                    2 weeks ago
+                  </p>
                 </div>
               </div>
               <p>
@@ -129,7 +147,7 @@ function Organisation() {
           </div>
         </div>
       </div>
-    </>
+    </section>
   );
 }
 
