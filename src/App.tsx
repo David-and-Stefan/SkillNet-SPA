9bed9bf80edf62606428eae1f266019cc7cc5b52
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2024-08-28 23:39:32 +0300

    WIP Organisation page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Routes } from 'react-router-dom';
 import Sidebar from './components/sidebar/Sidebar';
 import { ThemeProvider } from './contexts/ThemeProvider';
+import Organisation from './pages/organisation/Organisation';
 
 function App() {
   return (
     <ThemeProvider>
-      <main className="w-screen text-black dark:text-dark-300">
+      <main className="w-screen text-black dark:text-dark-300 flex justify-center align-middle">
         <Sidebar />
-
-        <Routes>
-          {/* <Route path={RoutePage.HOME} element={<Home />} /> */}
-        </Routes>
+        <div className="flex-1 p-10">
+          <Organisation />
+          <Routes>
+            {/* <Route path={RoutePage.HOME} element={<Home />} /> */}
+          </Routes>
+        </div>
       </main>
     </ThemeProvider>
   );
